dca83546276539271c49e31b5b4cd65edb82f507
anacsalmeida <anaalmeidatec@gmail.com>
2024-03-17 21:12:24 -0300

    Correção do context para camada de infra

--- CondoTech/Infra/Context/AppDbContext.cs
+++ CondoTech/Infra/Context/AppDbContext.cs
@@ -1,15 +1,13 @@
-﻿using CondoTech.Domain.Context;
-using CondoTech.Domain.Entities;
+﻿using CondoTech.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace CondoTech.Infra.Context
+namespace CondoTech.Domain.Context
 {
-    //public class AppDbContext : IAppDbContext<>
-    //{
-    //    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    //    {
-    //    }
-
-    //    public DbSet<User>? Users { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    //}
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+              : base(options)
+        { }
+        public DbSet<User>? Users { get; set; }
+    }
 }
