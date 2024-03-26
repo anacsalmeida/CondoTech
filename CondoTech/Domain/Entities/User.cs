5b879c87ef84654439ca6613842f0be8b29b7c07
anacsalmeida <anaalmeidatec@gmail.com>
2024-03-26 14:45:57 -0300

    Acrescentando atributos na entidade de user

--- CondoTech/Domain/Entities/User.cs
+++ CondoTech/Domain/Entities/User.cs
@@ -1,11 +1,29 @@
-﻿namespace CondoTech.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CondoTech.Domain.Entities
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public float Cpf {  get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string Cpf { get; set; }
+
+        public string AccessToken { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? LastLogin { get; set; }
+
+        public bool IsActive { get; set; } = true;
     }
 }
